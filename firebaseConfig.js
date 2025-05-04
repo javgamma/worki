c7f55ffdbd041e1ee08048f4a1e43877f64dcc22
javmirh <javmirdev@gmail.com>
2025-05-04 22:05:29 +0200

    Se cambia el nombre de la carpeta

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-const FirebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyD5sVNT2sWp6pHLNIYYMrGNBibVCZVXOzc",
   authDomain: "worki-4cbec.firebaseapp.com",
   projectId: "worki-4cbec",
@@ -12,7 +12,7 @@ const FirebaseConfig = {
   appId: "1:88823301843:web:b9901803dfef2d2e83b626",
 };
 
-const app = initializeApp(FirebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
